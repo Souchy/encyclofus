59887b361cd5381ae714a69ddd9fcbbe64720e0f
Souchy <robyn.g@hotmail.fr>
2022-10-03 18:58:52 -0400

    summon fixes + translate

--- src/pages/breeds/summon.ts
+++ src/pages/breeds/summon.ts
@@ -8,10 +8,16 @@ export class Summon {
 
 	@bindable
 	public summon: any;
+	@bindable
+	public side;
 
 	public constructor(db: db, @I18N private readonly i18n: I18N) {
 		this.db = db;
 	}
 
+	public get grade() {
+		// console.log("grades: " + JSON.stringify(this.summon.grades))
+		return this.summon.grades[this.side - 1];
+	}
 
 }
