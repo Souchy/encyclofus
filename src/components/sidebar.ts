a362d08492e18f7faff762c9f734a86ca26cabfc
Souchy <robyn.g@hotmail.fr>
2022-10-29 21:30:46 -0400

    fix sidebar translating bug

--- src/components/sidebar.ts
+++ src/components/sidebar.ts
@@ -3,13 +3,14 @@ import jsonBreeds from '../DofusDB/static/classes.json';
 
 import { inject } from 'aurelia';
 import { IRoute, IRouter, IRouteableComponent, ReloadBehavior, Navigation, Parameters, RoutingInstruction } from '@aurelia/router';
+import { I18N } from "@aurelia/i18n";
 
 @inject(db)
 export class sidebar {
 	public breeds: string[];
 	public db: db;
 
-	constructor(db: db) { 
+	constructor(db: db, @I18N private readonly i18n: I18N) {
 		this.db = db;
 		this.breeds = jsonBreeds.orderByIcon;
 	}
@@ -30,6 +31,10 @@ export class sidebar {
 	}
 	public get isRouteClass(): boolean {
 		return this.db.breedId > 0;
+	}
+
+	public translate(obj: any) {
+		return this.i18n.tr(obj as string);
 	}
 
 }
