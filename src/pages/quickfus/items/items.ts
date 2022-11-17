0ae34a48103b8d310f95e9888900b9695fd87dc9
Souchy <robyn.g@hotmail.fr>
2022-11-17 08:21:30 -0500

    items in zangodb, sort bugs

--- src/pages/quickfus/items/items.ts
+++ src/pages/quickfus/items/items.ts
@@ -8,12 +8,29 @@ export class items {
     public itemsPerPage: number = 50;
     public page:number = 0;
 
+    public items: Object[] = [];
+
     public constructor(readonly db: db, @IEventAggregator readonly ea: IEventAggregator) {
         this.ea.subscribe("quickfus:search", (filter: string) => this.updateSearch(filter));
+        this.updateSearch();
     }
 
-    public updateSearch(filter: string) {
-        
+    public async updateSearch(filter: string = "") {
+        this.db.items
+        .find({ level: 200 })
+        // .sort({ level: -1 })
+        // .sort(i => i.level)
+        // .sort({ level: 1 })
+        // .sort((a, b) => a.level > b.level)
+        .skip(0)
+        .limit(50)
+        .toArray();
+        // .forEach(i => {
+        //     console.log("i: " + i)
+        // })
+        // this.db.items.find({}).sort(i => i.level).skip(this.itemsPerPage * this.page).limit(this.itemsPerPage).toArray().then(a => {
+        //     this.items = a;
+        // })
     }
 
 
